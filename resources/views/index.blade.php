92caae164393ed490e8b579f57d8fc4aea5e5a8a
edmundasvtech <edmundas-paulius.kaminskas@stud.vilniustech.lt>
2023-08-04 00:30:36 +0300

    Bootstrap&jQuery

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layout')
 @section('title', 'Conference List')
 @section('content')
+    <div class="container">
+        <h1 class="display-4">Welcome to the Conference List</h1>
+
+        <ul class="list-group">
     @foreach($conferences as $conference)
         <h1>{{$conference['name']}}</h1>
         <p>{{$conference['description']}}</p>
@@ -9,4 +13,6 @@
         <p>{{$conference['end_date']}}</p>
         <br>
     @endforeach
+        </ul>
+    </div>
 @endsection
